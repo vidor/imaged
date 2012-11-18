a162a3f69312160059d58edddb09ac22cc7d4b7a
vidor <me@vidor.me>
2012-11-19 01:13:48 +0800

    fixloadmoreforie

--- wp-content/themes/imaged/templates/content.php
+++ wp-content/themes/imaged/templates/content.php
@@ -13,7 +13,7 @@
 <?php $total_posts = $wp_query->found_posts; ?>
 <div id="load-more">                
   <a data-total-posts="<?php echo $total_posts ; ?>" data-perpage="<?php echo get_option('posts_per_page');?>">
-    <span id="posts-count"> <?php echo get_option('posts_per_page').'/'. $total_posts; ?></span>                            
+     <span id="posts-count"> <?php echo get_option('posts_per_page').'/'. $total_posts; ?></span>                            
   </a>
 </div>
 
